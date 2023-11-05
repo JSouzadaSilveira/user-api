7f399af8f8da27d174869dca8e61d0040cc0dbe1
Jonathan Souza <jonathansouzadasilveira17@gmail.com>
2023-11-05 14:34:36 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import express from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { MongoClient } from "./database/mongo";
 
 const main = async () => {
@@ -12,10 +12,11 @@ const main = async () => {
   await MongoClient.connect();
 
   app.get("/users", async (req, res) => {
+    
     const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersControler = new GetUsersController(mongoGetUsersRepository);
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-    const { body, statusCode } = await getUsersControler.handle();
+    const { body, statusCode } = await getUsersController.handle();
     res.send(body).status(statusCode);
   });
 
--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,17 +1,18 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    // throw new Error("Method not implemented.");
+    const users = await MongoClient.db
+      .collection<MongoUser>("users")
+      .find({})
+      .toArray();
 
-    return [
-      {
-        firstName: "felipe",
-        lastName: "rocha",
-        email: "felipe@gmail.com",
-        password: "123",
-      },
-    ];
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
